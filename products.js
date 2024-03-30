fed79dedc1ed9a73df692428a70af3070380fc62
alihamdan2020 <ali_hamdane@hotmail.com>
2024-03-30 23:21:25 +0200

    change the way of onclick function in shop.js, note that if i have import as our example onclick as attribute does not working

---
+++ products.js
@@ -0,0 +1,34 @@
+export const products = [
+	{
+		productId: 1,
+		productName: "persil 2024",
+		productImg: "persil.jpg",
+		productPrice: 4.5,
+		productDesc: "this best ever cleaning soap for your clorful clothes",
+		productAvailable: true
+	},
+	{
+		productId: 2,
+		productName: "persil black",
+		productImg: "persil2.jpg",
+		productPrice: 2.5,
+		productDesc: "this best ever cleaning soap for your Abaya",
+		productAvailable: true
+	},
+	{
+		productId: 3,
+		productName: "product 3",
+		productImg: "img2.jpg",
+		productPrice: 4.5,
+		productDesc: "this is a product for winter",
+		productAvailable: false
+	},
+	{
+		productId: 4,
+		productName: "detol",
+		productImg: "detol.jpg",
+		productPrice: 6.5,
+		productDesc: "number 1 in lebanese market",
+		productAvailable: true
+	}
+];
--- shop.js
+++ shop.js
@@ -1,3 +1,4 @@
+import {products} from './products.js';
 let counter = document.querySelector(".counter");
 let mainContent = document.querySelector(".mainContent");
 
@@ -22,40 +23,7 @@ let USDollar = new Intl.NumberFormat('en-US', {
 const container = document.querySelector(".products");
 
 
-let products = [
-	{
-		productId: 1,
-		productName: "persil",
-		productImg: "persil.jpg",
-		productPrice: 4.5,
-		productDesc: "this best ever cleaning soap for your clorful clothes",
-		productAvailable: true
-	},
-	{
-		productId: 2,
-		productName: "persil black",
-		productImg: "persil2.jpg",
-		productPrice: 2.5,
-		productDesc: "this best ever cleaning soap for your Abaya",
-		productAvailable: true
-	},
-	{
-		productId: 3,
-		productName: "product 3",
-		productImg: "img2.jpg",
-		productPrice: 4.5,
-		productDesc: "this is a product for winter",
-		productAvailable: false
-	},
-	{
-		productId: 4,
-		productName: "detol",
-		productImg: "detol.jpg",
-		productPrice: 6.5,
-		productDesc: "number 1 in lebanese market",
-		productAvailable: true
-	}
-];
+
 
 /*
 since your variable has same name of obect property we can write
@@ -72,77 +40,54 @@ console.log(filteredArray);
 
 
 
-function fillProducts(product) {
+function fillProducts(product,i) {
 
+	
 	let productCard = document.createElement("div");
 	productCard.className = "productCard";
 
 	let productImg = document.createElement("img");
-	productImg.setAttribute("src", products[i].productImg);
+	productImg.setAttribute("src", product.productImg);
 	productCard.appendChild(productImg);
 
 
 
 	let productName = document.createElement("h3");
 	productName.setAttribute("class", "productText");
-	productName.textContent = products[i].productName;
+	productName.textContent = product.productName;
 	productCard.appendChild(productName);
 
 	let productDesc = document.createElement("p");
 	productDesc.setAttribute("class", "productText");
-	productDesc.textContent = products[i].productDesc;
+	productDesc.textContent = product.productDesc;
 	productCard.appendChild(productDesc);
 
 	let productPrice = document.createElement("p");
 	productPrice.className = "productText productPrice";
-	productPrice.textContent = USDollar.format(products[i].productPrice);
+	productPrice.textContent = USDollar.format(product.productPrice);
 	productCard.appendChild(productPrice);
 
-	let qtyHandler = document.createElement("div");
-	qtyHandler.className = "optionsHandler";
+	let inputQty=document.createElement("input");
+	inputQty.setAttribute("type","number");
+	inputQty.setAttribute("value","1");
+	inputQty.setAttribute("min","1");
+	inputQty.setAttribute("max","5");
+	productCard.appendChild(inputQty);
 
-	let optionQty1 = document.createElement("input");
-	optionQty1.setAttribute("type", "radio");
-	optionQty1.setAttribute("name", `qty${a}`);
-	optionQty1.setAttribute("checked", true);
-	optionQty1.setAttribute("value", 1);
-	let optionSpan1 = document.createElement("span");
-	optionSpan1.textContent = "1";
-
-
-	let optionQty2 = document.createElement("input");
-	optionQty2.setAttribute("type", "radio");
-	optionQty2.setAttribute("name", `qty${a}`);
-	optionQty2.setAttribute("value", 2);
-	let optionSpan2 = document.createElement("span");
-	optionSpan2.textContent = "2";
-
-	let optionQty3 = document.createElement("input");
-	optionQty3.setAttribute("type", "radio");
-	optionQty3.setAttribute("name", `qty${a}`);
-	optionQty3.setAttribute("value", 3);
-	let optionSpan3 = document.createElement("span");
-	optionSpan3.textContent = "3";
-
-	qtyHandler.appendChild(optionQty1);
-	qtyHandler.appendChild(optionSpan1);
-	qtyHandler.appendChild(optionQty2);
-	qtyHandler.appendChild(optionSpan2);
-	qtyHandler.appendChild(optionQty3);
-	qtyHandler.appendChild(optionSpan3);
-
+	
 	let addButton = document.createElement("button");
 	addButton.className = "btnAdd";
-	addButton.setAttribute("data-id", products[i].productId);
-	addButton.setAttribute("onclick", `addToCard(this,${a})`);
+	addButton.setAttribute("data-id", product.productId);
+	addButton.onclick=function(){addToCard(this)};
 	addButton.textContent = "add to card";
 
 	productCard.appendChild(addButton);
 
-	productCard.appendChild(qtyHandler);
-	a++;
 	return productCard;
 }
+
+
+
 
 let chooseOption;
 for (i = 0; i < products.length; i++) {
@@ -188,7 +133,7 @@ let bag =
 };
 
 
-function addToCard(btn, index) {
+function addToCard(btn) {
 	bag =
 	{
 		productId: 0,
@@ -204,16 +149,12 @@ function addToCard(btn, index) {
 	bag.productName = btn.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
 
 
-	let price = btn.previousElementSibling.textContent.replace("$", "");
+	let price = btn.previousElementSibling.previousElementSibling.textContent.replace("$", "");
 	bag.productPrice = parseFloat(price);
 
 	bag.productId = btn.getAttribute("data-id");
-
-	const productQty = document.querySelectorAll(`[name=qty${index}]`);
-	productQty.forEach(function (a) {
-		if (a.checked === true)
-			bag.productQty = a.value;
-	});
+	bag.productQty = btn.previousElementSibling.value;
+	
 
 	let exist = false;
 	for (let i = 0; i < allBag.length; i++) {
